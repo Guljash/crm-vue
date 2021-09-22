00a7cbe667ad24683481034e872a204ec8aaa862
Georgiy <guljash.main@gmail.com>
2021-09-22 17:55:40 +0300

    Initial dist subtree commit

---
+++ precache-manifest.4f99a6607889c0246e07c670c8f66488.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e198acf3ac53de3b988b",
+    "url": "/crm-vue/css/app.cc9dad12.css"
+  },
+  {
+    "revision": "5212a7235f7237f38da4",
+    "url": "/crm-vue/css/chunk-6fb2294d.507592f6.css"
+  },
+  {
+    "revision": "bf03cb30b4c4ebbb2eee",
+    "url": "/crm-vue/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "a8179d5d8fc40c3ddc6eaf7e102752cb",
+    "url": "/crm-vue/index.html"
+  },
+  {
+    "revision": "e198acf3ac53de3b988b",
+    "url": "/crm-vue/js/app.c780ae39.js"
+  },
+  {
+    "revision": "5d096ed6e07b960e0990",
+    "url": "/crm-vue/js/chunk-09858287.e92bbee8.js"
+  },
+  {
+    "revision": "e3f8f1f15813c7b18315",
+    "url": "/crm-vue/js/chunk-0fe47070.c34d23c7.js"
+  },
+  {
+    "revision": "81dd3014080950b54a6d",
+    "url": "/crm-vue/js/chunk-2d0d6d35.48b95d0b.js"
+  },
+  {
+    "revision": "70b76e5fd035ed107c10",
+    "url": "/crm-vue/js/chunk-2d2086b7.c990a93a.js"
+  },
+  {
+    "revision": "703454c1404be40aff6d",
+    "url": "/crm-vue/js/chunk-2d71fd2b.795eeed1.js"
+  },
+  {
+    "revision": "533b0060b0e2b0adcb4d",
+    "url": "/crm-vue/js/chunk-3fbbdc93.a938a2df.js"
+  },
+  {
+    "revision": "296be57fdc253b82c037",
+    "url": "/crm-vue/js/chunk-40378055.24698c60.js"
+  },
+  {
+    "revision": "e3ee291302ab88fd20d5",
+    "url": "/crm-vue/js/chunk-596b1de1.8208f670.js"
+  },
+  {
+    "revision": "5212a7235f7237f38da4",
+    "url": "/crm-vue/js/chunk-6fb2294d.be53bfd2.js"
+  },
+  {
+    "revision": "5dd4daa7a8498a1e0ed4",
+    "url": "/crm-vue/js/chunk-df2ff4d0.52188aaa.js"
+  },
+  {
+    "revision": "bf03cb30b4c4ebbb2eee",
+    "url": "/crm-vue/js/chunk-vendors.67e56ee7.js"
+  },
+  {
+    "revision": "f2443b2c1cd86f728afe8d5bad9b96a0",
+    "url": "/crm-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm-vue/robots.txt"
+  }
+]);
